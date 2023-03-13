2f6fde89e06bdc8f9e299b483432a3742f0d3fe6
disusen <55763781+disusen@users.noreply.github.com>
2023-03-13 20:02:42 +0200

    COTR-2 Fixes
    Fixed method names
    Changed the file location of menus
    Changed MainMenu and Settings root nodes to Control

---
+++ Scenes/UI/Menus/Main/MainMenu.cs
@@ -0,0 +1,49 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	private PackedScene settingsScene;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		settingsScene = (PackedScene)ResourceLoader.Load("res://Scenes/UI/Menus/Settings/Settings.tscn");
+	}
+	
+	/// <summary>
+	/// Button for starting a new game.
+	/// </summary>
+	private void ButtonStartNewPressed()
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/Maps/Main/Main.tscn");
+	}
+	
+
+	/// <summary>
+	/// Button for loading an existing save file.
+	/// </summary>
+	private void ButtonLoadPressed()
+	{
+		// Replace with function body.
+	}
+
+	/// <summary>
+	/// Button for accessing the settings menu.
+	/// </summary>
+	private void ButtonSettingsPressed()
+	{
+
+		Settings settingsScreen = settingsScene.Instantiate() as Settings;
+		AddChild(settingsScreen);
+		var control = GetNode<Control>("CanvasLayer/Control");
+		control.Hide();
+	}
+
+	/// <summary>
+	/// Button for quiting the game.
+	/// </summary>
+	private void ButtonQuitPressed()
+	{
+		GetTree().Quit();
+	}
+}
