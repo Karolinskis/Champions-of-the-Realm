0a7b5decced25e3d0fcb2e258bd4dc6a6da0b9e1
Karolis Paulavičius <k.paulavicius@ktu.edu>
2023-03-27 20:03:32 +0300

    COTR-32 Update Weapons manager
    Updated weapons manager to have the ability to use multiple types of weapons.

--- Scenes/Actors/Player/WeaponsManager.cs
+++ Scenes/Actors/Player/WeaponsManager.cs
@@ -1,15 +1,55 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 // Class dedicated for storing weapons with switching functionality
 public partial class WeaponsManager : Node2D
 {
-    public bool IsAttacking { get; set; } = false; // Is actor attacking
-    private Team.Teams team; // Team of the actor
-    public Weapon CurrentWeapon { get; set; } // weapon which actor is currently holding
+[Signal]
+	public delegate void WeaponChangedEventHandler(Weapon newWeapon);
+
+	/// <summary>
+	/// Current weapon the player is holding
+	/// </summary>
+	/// <value>Weapon object</value>
+	public Weapon CurrentWeapon { get; set; }
+
+	/// <summary>
+	/// All the weapons the player has
+	/// </summary>
+	public Weapon[] weapons;
+
+    /// <summary>
+    /// Check if the actor is currently attacking
+    /// </summary>
+	public bool IsAttacking { get; set; } = false;
+
+    // Team of the actor
+    private Team.Teams team;
+
+    // Time of attack
+	private Timer attackTimer;
+
     public override void _Ready()
     {
+        CurrentWeapon = GetNode<Weapon>("Melee");		
+		attackTimer = GetNode<Timer>("Melee/AttackTimer");
+
+        int weaponAmmount = 2;
+		Weapon[] weapons = GetChildren()
+			.OfType<Weapon>()
+            .Take(weaponAmmount)
+			.Select(weapon =>
+			{
+				weapon.Hide();
+				return weapon;
+			})
+			.ToArray();
+        
+        CurrentWeapon.Hide();
     }
+
     /// <summary>
     /// Method for initializing required values for weapons manager
     /// </summary>
@@ -21,15 +61,75 @@ public partial class WeaponsManager : Node2D
         weapon.Initialize(team);
         CurrentWeapon = weapon;
     }
+
     /// <summary>
-    /// Method for handeling weapon when idle
+    /// Add weapon to currently carrying weapon list
+    /// </summary>
+    /// <param name="index">Index to put in to</param>
+    public void AddWeapon(Weapon weapon, int index)
+    {
+        if (weapons[index] != null)
+            weapons[index].QueueFree();
+
+        AddChild(weapon);
+        weapons[index] = weapon;
+        weapon.Hide();
+    }
+
+    /// <summary>
+    /// Gives the current weapon the player is holding
+    /// </summary>
+    /// <returns>Returns the weapon object the player is holding</returns>
+    public Weapon GetCurrentWeapon()
+    {
+        return CurrentWeapon;
+    }
+
+    /// <summary>
+    /// Change the current weapon the user is holding
+    /// </summary>
+    public void ChangeWeapon()
+    {
+        int index = Array.IndexOf(weapons, CurrentWeapon);
+        for (int i = index; i < weapons.Length - 1; i++)
+        {
+            if (weapons[i + 1] != null)
+            {
+                SwitchWeapon(weapons[i + 1]);
+                return;
+            }
+        }
+        SwitchWeapon(weapons[0]);
+    }
+
+    /// <summary>
+    /// Switch weapon to the given weapon
+    /// </summary>
+    /// <param name="weapon">Weapon to switch to</param>
+    public void SwitchWeapon(Weapon weapon)
+    {
+        if (CurrentWeapon == weapon) return;
+        // Should cancel weapon attack
+        CurrentWeapon.Hide();
+        weapon.Show();
+        CurrentWeapon = weapon;
+        EmitSignal(nameof(WeaponChanged), CurrentWeapon);
+    }
+
+    /// <summary>
+    /// Method for handling weapon when idle
     /// </summary>
     public void Idle()
     {
         Rotation = 0;
+        if (CurrentWeapon is Melee melee)
+        {
+            melee.Idle();
+        }
     }
+
     /// <summary>
-    /// Method for handeling weapon when attacking
+    /// Method for handling weapon when attacking
     /// </summary>
     /// <param name="rotation"></param>
     /// <returns>If can attack</returns>
@@ -44,10 +144,103 @@ public partial class WeaponsManager : Node2D
         }
         return false;
     }
+
+    /// <summary>
+    /// Cancel the current attack
+    /// </summary>
+    public void CancelAttack()
+    {
+        IsAttacking = false;
+        Player player = GetParent() as Player;
+        player.PlayIdle();
+        //CurrentWeapon.StartCooldown();
+    }
+
+    /// <summary>
+    /// Delivers the damage
+    /// </summary>
+    public void Deliver()
+    {
+        if (IsAttacking)
+        {
+            if (CurrentWeapon is Melee melee && attackTimer.IsStopped())
+            {
+                IsAttacking = false;
+                melee.Deliver();
+            }
+        }
+    }
+
     /// <summary>
     /// Method for handeling weapon when walking
     /// </summary>
     public void Walking()
     {
+        if (CurrentWeapon is Melee melee)
+        {
+            melee.Walking();
+        }
+    }
+
+    /// <summary>
+    /// If the attack timer runs out it automaticaly delivers damage
+    /// </summary>
+    private void AttackTimerTimeout()
+    {
+        if (CurrentWeapon is Melee)
+        {
+            Deliver();
+        }
+    }
+
+    /// <summary>
+    /// Save the current weapon loadout
+    /// </summary>
+    /// <returns>Returns the dictionary of all weapons currently in the loadout</returns>
+    public Dictionary<string, Variant> Save()
+    {
+        Dictionary<string, Variant> data = new Dictionary<string, Variant>();
+
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            string itemid = "Item" + i;
+
+            if (weapons[i] != null)
+            {
+                if (weapons[i] is Melee)
+                {
+                    data.Add(itemid + "Type", "Melee");
+                    data.Add(itemid + "Filename", weapons[i].Name);
+                }
+            }
+            else
+            {
+                data.Add(itemid + "Type", "None");
+            }
+        }
+
+        return data;
+    }
+
+    /// <summary>
+    /// Load the current weapon loadout
+    /// </summary>
+    /// <param name="data">Loads all the saved weapons to the loadout</param>
+    public void Load(Dictionary<string, Variant> data)
+    {
+        Weapon setWeapon = new Weapon();
+
+        for (int i = 0; i < weapons.Length; i++)
+        {
+            string itemid = "Item" + i;
+
+            if ((string)data[itemid + "Type"] == "Melee")
+            {
+                var newObjectScene = (PackedScene)ResourceLoader.Load(data[itemid + "Filename"].ToString());
+                CurrentWeapon = newObjectScene.Instantiate() as Weapon;
+                AddWeapon(CurrentWeapon, i);
+            }
+        }
+        Initialize(Team.Teams.PLAYER, CurrentWeapon);
     }
 }
