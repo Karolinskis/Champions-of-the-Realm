981bc30729839669968cdb773992ada745c8bac9
Elijuszek <56361945+Elijuszek@users.noreply.github.com>
2023-03-30 12:24:37 +0300

    COTR-39 Implemented save/load functionality
    -Implemented parsing save file in Globals class.
    -Added main.tscn scene to "Persist" group
    -Started implementing map.cs save/load functionality

--- Scenes/Globals/Globals.cs
+++ Scenes/Globals/Globals.cs
@@ -1,15 +1,87 @@
 using Godot;
+using Godot.Collections;
 using System;
 
+/// <summary>
+/// Class for handeling global actions (Scene switching, save/load functionality)
+/// </summary>
 public partial class Globals : Node
 {
-	// Called when the node enters the scene tree for the first time.
+	private const string SaveDir = "user://saves/"; // save files directory path
+	private string savePath = SaveDir + "save.dat"; // save file path
+	public enum LoadingForm
+	{
+		New,
+		Save,
+		Load
+	}
+	public LoadingForm loadingForm { get; set; } = LoadingForm.New;
+
+
 	public override void _Ready()
 	{
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	//public override void _Process(double delta)
-	//{
-	//}
+	public bool SaveGame()
+	{
+		if (!DirAccess.DirExistsAbsolute(SaveDir))
+		{
+			DirAccess.MakeDirRecursiveAbsolute(SaveDir);
+		}
+		// opening file and encrpypting filewith password
+		FileAccess saveFile = FileAccess.OpenEncryptedWithPass(savePath, FileAccess.ModeFlags.Write, "nekompiliuoja");
+		if (saveFile.GetError() != Error.Ok)
+		{
+			GD.PushError("Failed to save data!"); // for debuging purposes
+			return false;
+		}
+		Array<Node> saveNodes = GetTree().GetNodesInGroup("Persist");
+		foreach (Node saveNode in saveNodes)
+		{
+			if (!saveNode.HasMethod("Save") || saveNode.Name.IsEmpty)
+			{
+				continue;
+			}
+			Variant data = saveNode.Call("Save"); // Getting dictionary from every Persist node
+			GD.Print(data); // for debugging purposes
+			saveFile.StoreLine(Json.Stringify(data));
+		}
+		return true;
+	}
+
+	public async void LoadGame()
+	{
+		if (!FileAccess.FileExists(savePath))
+		{
+			return;
+		}
+
+		Array<Node> saveNodes = GetTree().GetNodesInGroup("Presist"); // getting all unwanted save nodes in current scene
+		foreach (Node saveNode in saveNodes)
+		{
+			saveNode.QueueFree(); // removing all unwanted save nodes
+		}
+		GetTree().CurrentScene.QueueFree();
+		if (saveNodes.Count > 0)
+		{
+			// Giving small amount of time to fully remove all unwanted nodes
+			await ToSignal(GetTree().CreateTimer(0.00001f), "timeout"); // TODO implement better solution, without async
+		}
+		FileAccess saveFile = FileAccess.OpenEncryptedWithPass(savePath, Godot.FileAccess.ModeFlags.Read, "nekompiliuoja");
+		if (saveFile.GetError() != Error.Ok) // Checking if saveFile was loaded without any errors
+		{
+			GD.PushError("Failed to load data!");
+		}
+		while (saveFile.GetPosition() < saveFile.GetLength()) // reading every line
+		{
+			Dictionary<string, Variant> data = (Dictionary<string, Variant>)Json.ParseString(saveFile.GetLine());
+			PackedScene newObjectScene = ResourceLoader.Load<PackedScene>(data["Filename"].ToString());
+			Node newObject = newObjectScene.Instantiate();
+			newObject.Set("position", new Vector2((float)data["PosX"], (float)data["PosY"]));
+			GetNode(data["Parent"].ToString()).AddChild(newObject);
+			newObject.Call("Load", data);
+		}
+		GetTree().CurrentScene.Call("LoadSavedPlayer");
+	}
+
 }
--- Scenes/Maps/Main/Map.cs
+++ Scenes/Maps/Main/Map.cs
@@ -5,70 +5,71 @@ using Godot;
 /// </summary>
 public partial class Map : Node2D
 {
-    private Player player; // Player in the scene
-    private PackedScene playerScene; // Player resource
-    private PackedScene PauseMenuScene; // Pause menu resource
-    private PackedScene gameOverScene;  // gameOver resource
-    private GUI hud; // GUI in the scene
-    private Camera2D camera; // Player camera
-    private TileMap ground; // Ground level
+	
+	private Player player; // Player in the scene
+	private PackedScene playerScene; // Player resource
+	private PackedScene PauseMenuScene; // Pause menu resource
+	private PackedScene gameOverScene;  // gameOver resource
+	private GUI hud; // GUI in the scene
+	private Camera2D camera; // Player camera
+	private TileMap ground; // Ground level
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        // Loading resources
-        playerScene = ResourceLoader.Load<PackedScene>("res://Scenes/Actors/Player/Player.tscn");
-        gameOverScene = ResourceLoader.Load<PackedScene>("res://Scenes/UI/Menus/GameOver/GameOver.tscn");
-        PauseMenuScene = ResourceLoader.Load<PackedScene>("res://Scenes/UI/Menus/Pause/PauseMenu.tscn");
-        
-        // Loading nodes
-        hud = GetNode<GUI>("HUD");
-        player = GetNode<Player>("Player");
-        camera = GetNode<Camera2D>("Camera");
-        ground = GetNode<TileMap>("TileMapGround");
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// Loading resources
+		playerScene = ResourceLoader.Load<PackedScene>("res://Scenes/Actors/Player/Player.tscn");
+		gameOverScene = ResourceLoader.Load<PackedScene>("res://Scenes/UI/Menus/GameOver/GameOver.tscn");
+		PauseMenuScene = ResourceLoader.Load<PackedScene>("res://Scenes/UI/Menus/Pause/PauseMenu.tscn");
+		
+		// Loading nodes
+		hud = GetNode<GUI>("HUD");
+		player = GetNode<Player>("Player");
+		camera = GetNode<Camera2D>("Camera");
+		ground = GetNode<TileMap>("TileMapGround");
 
-        // Setting signals
-        player.Connect("PlayerHealthChanged", new Callable(hud, "ChangeCurrentHealth"));
-        player.Connect("PLayerGoldChanged", new Callable(hud, "ChangeCurrency"));
-        player.Connect("PlayerMaxHealthChanged", new Callable(hud, "ChangeMaxHealth"));
-        player.Connect("PlayerDied", new Callable(this, "ShowGameOver"));
-        player.Connect("PlayerXpChanged", new Callable(hud, "ChangeXP"));
-        hud.Initialize(player.Stats);
+		// Setting signals
+		player.Connect("PlayerHealthChanged", new Callable(hud, "ChangeCurrentHealth"));
+		player.Connect("PLayerGoldChanged", new Callable(hud, "ChangeCurrency"));
+		player.Connect("PlayerMaxHealthChanged", new Callable(hud, "ChangeMaxHealth"));
+		player.Connect("PlayerDied", new Callable(this, "ShowGameOver"));
+		player.Connect("PlayerXpChanged", new Callable(hud, "ChangeXP"));
+		hud.Initialize(player.Stats);
 
-        // Setthing camera
-        player.SetCameraTransform(camera.GetPath());
-        SetCameraLimits();
-    }
+		// Setthing camera
+		player.SetCameraTransform(camera.GetPath());
+		SetCameraLimits();
+	}
 
-    /// <summary>
-    /// Pauses this scene
-    /// </summary>
-    public void Pause()
-    {
-        PauseMenu pauseMenu = PauseMenuScene.Instantiate() as PauseMenu;
-        AddChild(pauseMenu);
-    }
+	/// <summary>
+	/// Pauses this scene
+	/// </summary>
+	public void Pause()
+	{
+		PauseMenu pauseMenu = PauseMenuScene.Instantiate() as PauseMenu;
+		AddChild(pauseMenu);
+	}
 
-    /// <summary>
-    /// Realoading scene after player death
-    /// </summary>
-    public void ShowGameOver()
-    {
-        GameOver gameOver = gameOverScene.Instantiate() as GameOver;
-	AddChild(gameOver);
-    }
+	/// <summary>
+	/// Realoading scene after player death
+	/// </summary>
+	public void ShowGameOver()
+	{
+		GameOver gameOver = gameOverScene.Instantiate() as GameOver;
+		AddChild(gameOver);
+	}
 
-    /// <summary>
-    /// Method for setting camera limits according to map size
-    /// </summary>
-    protected void SetCameraLimits()
-    {
-        Rect2 mapLimits = ground.GetUsedRect();
-        Vector2 mapCellSize = ground.TileSet.TileSize;
-        // 96px (two tiles of obstacle tilemap)
-        camera.LimitLeft = (int)(mapLimits.Position.X * mapCellSize.X) - 96;
-        camera.LimitRight = (int)(mapLimits.End.X * mapCellSize.X) - 96;
-        camera.LimitTop = (int)(mapLimits.Position.Y * mapCellSize.Y) - 96;
-        camera.LimitBottom = (int)(mapLimits.End.Y * mapCellSize.Y) - 96;
-    }
+	/// <summary>
+	/// Method for setting camera limits according to map size
+	/// </summary>
+	protected void SetCameraLimits()
+	{
+		Rect2 mapLimits = ground.GetUsedRect();
+		Vector2 mapCellSize = ground.TileSet.TileSize;
+		// 96px (two tiles of obstacle tilemap)
+		camera.LimitLeft = (int)(mapLimits.Position.X * mapCellSize.X) - 96;
+		camera.LimitRight = (int)(mapLimits.End.X * mapCellSize.X) - 96;
+		camera.LimitTop = (int)(mapLimits.Position.Y * mapCellSize.Y) - 96;
+		camera.LimitBottom = (int)(mapLimits.End.Y * mapCellSize.Y) - 96;
+	}
 }
