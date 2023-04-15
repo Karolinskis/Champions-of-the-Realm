5031685bc95dbe8f6574fa4bf1fc7d2ecff0ea99
disusen <55763781+disusen@users.noreply.github.com>
2023-04-15 23:48:28 +0300

    COTR-68 Added comments for ObjectPool.cs methods

--- Scenes/Globals/ObjectPool.cs
+++ Scenes/Globals/ObjectPool.cs
@@ -7,16 +7,24 @@ using System.Collections.Concurrent;
 /// <typeparam name="T">Any object with IPoolable interface</typeparam>
 public partial class ObjectPool<T> where T : IPoolable, new()
 {
-    private readonly ConcurrentBag<T> items;
-    private int counter = 0;
-    private int max;
+    private readonly ConcurrentBag<T> items; // items.
+    private int counter = 0; // items currently in the pool.
+    private int max; // maximum amount of items in a pool.
 
+    /// <summary>
+    /// Method for creating object pool and setting the max amount of items in a pool.
+    /// </summary>
+    /// <param name="max">Pool size</param>
     public ObjectPool(int max = 100)
     {
         this.items = new ConcurrentBag<T>();
         this.max = max;
     }
 
+    /// <summary>
+    /// Method for returning objects to the pool.
+    /// </summary>
+    /// <param name="item">item to be returned.</param>
     public void Release(T item)
     {
         if (counter < max)
@@ -25,6 +33,10 @@ public partial class ObjectPool<T> where T : IPoolable, new()
             counter++;
         }
     }
+
+    /// <summary>
+    /// Method for getting an item from the pool..
+    /// </summary>
     public T Get()
     {
         T item;
@@ -40,6 +52,5 @@ public partial class ObjectPool<T> where T : IPoolable, new()
             counter++;
             return obj;
         }
-
     }
 }
