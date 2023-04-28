9e99899e3252e9215dffbf70bb16a7b0ad5a534c
Arnelys <arnas1373@gmail.com>
2023-04-28 22:13:36 +0300

    COTR-82 Removed usage of var, formatted code

--- Scenes/Utility/EnemySpawner.cs
+++ Scenes/Utility/EnemySpawner.cs
@@ -23,20 +23,20 @@ public partial class EnemySpawner : Node2D
     private float limitTop = 20f; //map coordinates limit top
     private float limitBottom; //map coordinates limit bottom
 
-	/// <summary>
-	/// Called when the node enters the scene tree for the first time.
-	/// </summary>
-	public override void _Ready()
+    /// <summary>
+    /// Called when the node enters the scene tree for the first time.
+    /// </summary>
+    public override void _Ready()
     {
         base._Ready();
         timer = GetNode<Timer>("Timer");
     }
 
-	/// <summary>
-	/// Initialize method for initializing limit values
-	/// </summary>
-	/// <param name="vector">Vector with limit values</param>
-	public void Initialize(Vector2 vector, Player player)
+    /// <summary>
+    /// Initialize method for initializing limit values
+    /// </summary>
+    /// <param name="vector">Vector with limit values</param>
+    public void Initialize(Vector2 vector, Player player)
     {
         limitRight = vector.X; //Setting right limit of the map
         limitBottom = vector.Y; //Setting bottom limit of the map
@@ -64,18 +64,18 @@ public partial class EnemySpawner : Node2D
     private void OnTimerTimeout()
     {
         time++;
-        if(time >= 10 && timer.WaitTime > 0.2) //Every 10 seconds the timer wait time is decreased so that enemies spawn more often
+        if (time >= 10 && timer.WaitTime > 0.2) //Every 10 seconds the timer wait time is decreased so that enemies spawn more often
         {
             time = 0;
             timer.WaitTime -= 0.1;
         }
 
-        var random = new Random();
+        Random random = new Random();
         int spawn = random.Next(0, Enemies.Count); //Generating random index of which enemy to spawn
 
-        var enemyScene = Enemies[spawn].Instantiate<Infantry>();
-		//TODO: enemyScene.AI. 
-		enemyScene.GlobalPosition = GetRandomPosition();
-		AddChild(enemyScene);
+        Infantry enemyScene = Enemies[spawn].Instantiate<Infantry>();
+        //TODO: enemyScene.AI. 
+        enemyScene.GlobalPosition = GetRandomPosition();
+        AddChild(enemyScene);
     }
 }
